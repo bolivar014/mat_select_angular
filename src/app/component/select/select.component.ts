392561ce7a5632174b60481f88da7009326bf163
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-09-04 19:36:31 -0500

    autocomplete country

--- src/app/component/select/select.component.ts
+++ src/app/component/select/select.component.ts
@@ -9,12 +9,15 @@ import { map, startWith } from 'rxjs/operators';
   styleUrls: ['./select.component.css']
 })
 export class SelectComponent implements OnInit {
-  myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
+  // myControl = new FormControl('');
+  myControlCountry = new FormControl('');
+  // options: string[] = ['Colombia','Brazil','Perú','Argentina'];
+  optionsCountry: string[] = ['Colombia','Brazil','Perú','Argentina'];
   filteredOptions?: Observable<string[]>;
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+    // this.filteredOptions = this.myControl.valueChanges.pipe(
+    this.filteredOptions = this.myControlCountry.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value || '')),
     );
@@ -23,6 +26,7 @@ export class SelectComponent implements OnInit {
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    // return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    return this.optionsCountry.filter(optionsCountry => optionsCountry.toLowerCase().includes(filterValue));
   }
 }
